e806624bdaf00e126de2a747a24a1acbe2684cb9
Jamie-19 <mdl21cs057@mec.ac.in>
2024-07-10 12:41:49 +0530

    wow

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -1,12 +1,23 @@
 const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
+const modalDiv = document.querySelector('.popup-modal');
+const backdrop = document.querySelector('.backdrop');
+
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop');
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+  modalDiv.classList.remove('close');
+  backdrop.classList.remove('close');
+  modalDiv.classList.add('show');
+  backdrop.classList.add('show');
+}
+
+function closeModal() {
+  modalDiv.classList.remove('show');
+  backdrop.classList.remove('show');
+  modalDiv.classList.add('close');
+  backdrop.classList.add('close');
+
 }
 
 openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', closeModal);
